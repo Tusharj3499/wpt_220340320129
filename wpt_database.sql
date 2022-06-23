b0a147766e22761f9cf91fff8ee675364cd5ec22
Tusharj3499 <100940895+Tusharj3499@users.noreply.github.com>
2022-06-23 12:16:17 +0530

    Add files via upload

---
+++ wpt_database.sql
@@ -0,0 +1,19 @@
+create table book
+(
+bookid integer primary key,
+bookname varchar(15),
+price integer
+);
+
+
+desc book;
+
+select * from book;
+
+insert into book value
+(1,"javascript",1000),
+(2,"java",2000),
+(3,"ajax",3000),
+(4,"react",4000);
+
+
